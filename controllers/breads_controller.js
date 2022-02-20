6e2e294ff95f3cd2491a1fd26615cbfb21bde750
Wajih Hassouneh <wajihmohd>
2022-02-20 07:34:14 -0500

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,12 +3,14 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 
  // INDEX
-breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread
-      }
-    )
+ breads.get('/', (req, res) => {
+  res.render('Index',
+    {
+      breads: Bread,
+      title: 'Index Page'
+    }
+  )
+})
   // res.send(Bread)
 })
 module.exports = breads
