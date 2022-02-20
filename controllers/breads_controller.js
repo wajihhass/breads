a938aee6d541abc5635fa1c424c4eaf2684a1f15
Wajih Hassouneh <wajihmohd>
2022-02-20 07:22:19 -0500

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,13 +2,19 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
-// INDEX
+ // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+  // res.send(Bread)
 })
-
 module.exports = breads
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
   })
+  /*88888*/
+  
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 // ROUTES
 app.get('/', (req, res) => {
